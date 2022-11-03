cc27fe011d64a4542fe407b84209dc0ac8c907d8
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-11-02 19:17:30 -0500

    added about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,15 @@ function About() {
     <section className="about">
       <h2 className='section-title'>Who am I?</h2>
       <img src={self} style={{width:"30%"}} className="profile-pic" alt="Joe in blue shirt"/>
-      <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
+      <p>Full stack web developer leveraging a background in pharmaceuticals and data analysis to focus
+on data integrity. Recently earned a certificate in full stack web development from the University
+of Minnesota, with newly developed skills in JavaScript, CSS, React.js, MongoDB, SQL, and
+responsive web design. Known as an innovative problem solver that is always looking for ways
+to improve processes and boost efficiency. Each project presents an opportunity to learn
+something new. Recently worked on a team of four to develop a single-page MERN app that
+connects users to handyman providers. I am excited to test and grow my skills as part of a
+fast-paced, quality-driven team to build better experiences on the web.
+</p>
     </section>
   );
 }
